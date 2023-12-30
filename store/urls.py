9209242b6d1923c6653fb595ad99b4cab8377e82
Vahe Hovhannisyan <work@vahehovhannisyan.link>
2023-12-30 19:26:39 +0400

    added blog and products pages

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 import store.views as views
 
+# Serving media files during development
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.home, name='home'),
-]
+    path('blog/', views.blog, name='blog'),
+    path('product/', views.product, name='products'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Blog, Product
 
 
 def home(request):
     return render(request, 'store/home.html')
+
+
+def blog(request):
+    blogs = Blog.objects.all()
+    return render(request, 'store/blog.html', {'blogs': blogs})
+
+
+def product(request):
+    products = Product.objects.all()
+    return render(request, 'store/products.html', {'products': products})
