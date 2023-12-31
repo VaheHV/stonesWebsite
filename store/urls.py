9ac3538eac8474f0bf797aeb65af090fa51a8959
Vahe Hovhannisyan <work@vahehovhannisyan.link>
2023-12-31 18:13:26 +0400

    added individual pages for blogs and products

--- store/urls.py
+++ store/urls.py
@@ -9,6 +9,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('blog/', views.blog, name='blog'),
-    path('product/', views.product, name='products'),
+    path('product/', views.products, name='products'),
     path('about/', views.about, name='about'),
+    path('blog/<int:pk>/', views.article_page, name='article'),
+    path('product/<int:pk>/', views.product_page, name='product'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -3,7 +3,8 @@ from .models import Blog, Product
 
 
 def home(request):
-    return render(request, 'store/home.html')
+    blogs = Blog.objects.all()[:4]
+    return render(request, 'store/home.html', {'blogs': blogs})
 
 
 def blog(request):
@@ -11,10 +12,20 @@ def blog(request):
     return render(request, 'store/blog.html', {'blogs': blogs})
 
 
-def product(request):
-    products = Product.objects.all()
-    return render(request, 'store/products.html', {'products': products})
+def products(request):
+    products_all = Product.objects.all()
+    return render(request, 'store/products.html', {'products': products_all})
 
 
 def about(request):
     return render(request, 'store/about.html')
+
+
+def article_page(request, pk):
+    article = Blog.objects.get(id=pk)
+    return render(request, 'store/article.html', {'article': article})
+
+
+def product_page(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'store/product_page.html', {'product': product})
