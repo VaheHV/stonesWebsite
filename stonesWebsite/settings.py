77eade991039649ac814de89303dc9dcd10ccea8
Vahe Hovhannisyan <work@vahehovhannisyan.link>
2024-01-06 16:12:55 +0400

    bug fixes

--- stonesWebsite/settings.py
+++ stonesWebsite/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".pythonanywhere.com"]
+ALLOWED_HOSTS = [".pythonanywhere.com", 'localhost', '127.0.0.1']
 
 
 # Application definition
